9403eb6f217b2a0db289c58309b2c715e976eb15
Treemo <treemo@hotmail.fr>
2014-06-15 13:35:45 +0200

    ModuleManager: add method action + rename variable

--- core/ModuleManager.py
+++ core/ModuleManager.py
@@ -6,6 +6,16 @@ from core import Log
 import os
 
 __module_list = {} # module data
+__action_method_list = []
+
+def add_method_action(method_name, handle_start=None, handle_stop=None):
+    """Add a method action.
+    """
+    __action_method_list.append({
+        'method_name':  method_name,
+        'handle_start': handle_start,
+        'handle_stop':  handle_stop,
+    })
 
 
 def call_module_method(module_name, method_name, *arg):
@@ -23,39 +33,38 @@ def call_module_method(module_name, method_name, *arg):
     return getattr(__module_list[module_name]['instance'], method_name)(*arg)
 
 
-def get(name):
+def get(module_name):
     """Get module instance.
     """
-    return __module_list[name]['instance'] if name in __module_list else None
+    return __module_list[module_name]['instance'] if module_name in __module_list else None
 
 
-def load(name):
+def load(module_name):
     """load module.
     """
     from core import Daemon
-    dir_path = '%s%s/' % (Daemon.MODULES_PATH, name)
+    dir_path = '%s%s/' % (Daemon.MODULES_PATH, module_name)
     if not os.path.isdir(dir_path):
         return False
 
-    if name in __module_list and not __module_list[name]['instance']:
+    if module_name in __module_list and not __module_list[module_name]['instance']:
         module_path = '%sModule.py' % dir_path
-        if os.path.isfile(module_path):
+        if not os.path.isfile(module_path):
             return False
 
         try:
-            __module_list[name]['instance'] = __import__(
-                'modules.' + name + '.Module',
+            __module_list[module_name]['instance'] = __import__(
+                'modules.' + module_name + '.Module',
                 globals(),
                 locals(),
                 ['Module'],
-                -1
             ).Module()
 
         except ImportError as e:
-            Log.error('Import error, module %s (%s)' % (name, e))
+            Log.error('Import error, module %s (%s)' % (module_name, e))
             return False
 
-    call_module_method(name, 'load_configuration')
+    call_module_method(module_name, 'load_configuration')
 
     return True
 
@@ -66,26 +75,29 @@ def load_all():
     __load_module_list()
     nb = 0
 
-    for name in __module_list:
-        if not __module_list[name]['disable']:
-            load(name)
+    for module_name in __module_list:
+        if not __module_list[module_name]['disable'] and load(module_name):
             nb += 1
 
     Log.debug('%d modules loaded' % nb)
 
 
-def restart(name):
+def restart(module_name):
     """Restart module.
     """
-    Log.debug('restart modules %s' % name)
-    stop(name)
-    start(name)
+    Log.debug('restart modules %s' % module_name)
+    stop(module_name)
+    start(module_name)
 
 
-def start(name):
+def start(module_name):
     """Start module.
     """
-    # todo
+    if not module_name in __module_list or not __module_list[module_name]['instance']:
+        return False
+
+    __call_method_action(module_name, 'handle_start')
+
     return True
 
 
@@ -95,19 +107,21 @@ def start_all():
     __load_module_list()
     nb = 0
 
-    for name in __module_list:
-        if not __module_list[name]['disable']:
-            start(name)
+    for module_name in __module_list:
+        if not __module_list[module_name]['disable'] and start(module_name):
             nb += 1
 
     Log.debug('%d modules started' % nb)
 
 
-def stop(name):
+def stop(module_name):
     """Stop module.
     """
-    Log.debug('stop modules %s' % name)
-    # todo
+    Log.debug('stop modules %s' % module_name)
+
+    __call_method_action(module_name, 'handle_stop')
+
+    return True
 
 
 def stop_all():
@@ -115,12 +129,34 @@ def stop_all():
     """
     nb = 0
 
-    for name in __module_list:
-        if __module_list[name]['instance']:
-            stop(name)
+    for module_name in __module_list:
+        if __module_list[module_name]['instance'] and stop(module_name):
             nb += 1
 
     Log.debug('%d modules stoped' % nb)
+
+
+def __call_method_action(module_name, handle_name):
+    """Call a method action.
+    """
+    if not module_name in __module_list:
+        return False
+
+    if not __module_list[module_name]['instance']:
+        return False
+
+    if not handle_name in ['handle_start', 'handle_stop']:
+        return False
+
+    instance = __module_list[module_name]['instance']
+
+    for method_name in dir(instance):
+        for action_method in __action_method_list:
+            if method_name.startswith(action_method['method_name']):
+                action_method[handle_name](
+                    module_name,
+                    getattr(instance, method_name)
+                )
 
 
 def __load_module_list():
@@ -136,9 +172,12 @@ def __load_module_list():
     dir_list = sorted(os.listdir(Daemon.MODULES_PATH))
     nb = 0
 
-    for name in dir_list:
-        if os.path.isdir('%s%s' % (Daemon.MODULES_PATH, name)):
-            __module_list[name] = {
+    for module_name in dir_list:
+        if os.path.isdir('%s%s' % (Daemon.MODULES_PATH, module_name)):
+            if '__pycache__' in module_name:
+                continue
+
+            __module_list[module_name] = {
                 'instance': None,
                 'disable':  False,
             }
