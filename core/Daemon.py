1b48442992ba09a66b4bd71dfc02af195ab29dcb
Treemo <treemo@hotmail.fr>
2014-09-08 16:02:39 +0200

    add restart daemon

--- core/Daemon.py
+++ core/Daemon.py
@@ -14,12 +14,18 @@ ROOT_PATH = '%s/' % os.sep.join(__file__.split(os.sep)[:-2])
 CONFIGS_PATH = '%sconfigs/' % ROOT_PATH
 MODULES_PATH = '%smodules/' % ROOT_PATH
 
+def launch_daemon():
+    """start new application.
+    """
+    return os.system('"%sdaemon.py" 1> /dev/null 2>&1 &' % ROOT_PATH) == 0
+
 
 def restart():
     """Restart daemon.
     """
-    stop(name)
-    #start(name) todo start new aplication
+    stop()
+    launch_daemon()
+    
 
 
 def start():
