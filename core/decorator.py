9f441dadda83d28913e50b6064af6228f22fd66a
Treemo <treemo@hotmail.fr>
2014-09-15 12:51:51 +0200

    add thread decorator

--- core/decorator.py
+++ core/decorator.py
@@ -1,1 +1,8 @@
-__author__ = 'treemo'
+from circuits import task, Worker
+
+
+def threaded(f):
+    def inner(self, *args, **kwargs):
+        Worker().register(self)
+        self.fire(task(f, self, *args, **kwargs))
+    return inner
