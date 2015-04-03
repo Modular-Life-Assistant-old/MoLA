3d25199990b12026f02e7b915fa4f3d5c66ed803
Treemo <treemo@hotmail.fr>
2015-04-03 10:55:24 +0200

    add "mutex" decorator

--- core/decorator.py
+++ core/decorator.py
@@ -7,6 +7,24 @@ def async(f):
     return inner
 
 
+def mutex(f):
+    def inner(self, *args, **kwargs):
+        key = '%s-%s-%s' % (str(f), str(args), str(kwargs))
+
+        if hasattr(self, 'mutex_decorator'):
+            while key in self.mutex_decorator:
+                yield
+
+            self.mutex_decorator.append(key)
+
+        else:
+            self.mutex_decorator = [key]
+
+        yield from f(self, *args, **kwargs)
+        self.mutex_decorator.remove(key)
+    return inner
+
+
 def threaded(f):
     def inner(self, *args, **kwargs):
         self.fire(task(f, self, *args, **kwargs), 'worker')
