e611e2db52f0102731c5f9574d9b6336844803b9
Treemo <treemo@hotmail.fr>
2015-12-03 18:28:51 +0100

    network helper: add linux arp

--- helpers/network.py
+++ helpers/network.py
@@ -1,9 +1,51 @@
-import socket
-from struct import pack
+import os
 import subprocess
+from core import Log
 
 
 def get_arp_infos():
+    """Get arp infos.
+
+    :return: arp infos dict
+    """
+
+    if os.name == 'nt':
+        return _get_arp_infos_windows()
+
+    return _get_arp_infos_linux()
+
+
+def _get_arp_infos_linux():
+    """Get arp infos.
+
+    :return: arp infos dict
+    """
+    result = {}
+
+    # ping sweep
+    try:
+        subprocess.check_output('ping 255.255.255.255 -b -c 1', shell=True)
+    except subprocess.CalledProcessError as e:
+        Log.error('Network helper: ping sweep: %s' % e)
+
+    output = subprocess.check_output('arp -e', shell=True)
+    if not output:
+        return result
+
+    for line in output.split(b'\n'):
+        if not line or b'gateway' in line or b'incomplete' in line:
+            continue
+
+        data = line.split()
+        result[data[0].decode()] = {
+            'mac': data[2].decode().replace(':', ''),
+            'is_dynamic': True,  # entry are remove on inactive
+        }
+
+    return result
+
+
+def _get_arp_infos_windows():
     """Get arp infos.
 
     :return: arp infos dict
@@ -12,7 +54,7 @@ def get_arp_infos():
 
     output = subprocess.check_output('arp -a', shell=True)
     if not output:
-        return output
+        return result
 
     for line in output.split(b'\n'):
         if not line or len(line.split()) != 3:
