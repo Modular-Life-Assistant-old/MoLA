bee48acd2f57a2dbc0c3cf7c66c138b8440bf89f
Treemo <treemo@hotmail.fr>
2015-12-05 19:02:51 +0100

    Camera device: add flag

--- helpers/devices/CameraDevice.py
+++ helpers/devices/CameraDevice.py
@@ -9,9 +9,19 @@ from io import BytesIO
 from helpers.devices.BaseDevice import BaseDevice
 
 
+CAMERA_STREAMING_FLAG   = 1
+CAMERA_MOVE_TOP_FLAG    = 2
+CAMERA_MOVE_BOTTOM_FLAG = 4
+CAMERA_MOVE_RIGHT_FLAG  = 8
+CAMERA_MOVE_LEFT_FLAG   = 12
+CAMERA_ZOOM_IN_FLAG     = 24
+CAMERA_ZOOM_OUT_FLAG    = 48
+
+
 class CameraDevice(BaseDevice):
     motion_threshold = 6.0
     snapshot_cache = 1  # seconde
+    flags = 0
     __snapshot = None
     __snapshot_timestamp = None
 
@@ -27,12 +37,60 @@ class CameraDevice(BaseDevice):
 
         self.make_snapshot = make_snapshot_decorator(self.make_snapshot)
 
+        # set flag
+        assoc = (
+            (self.get_streaming, CAMERA_STREAMING_FLAG),
+            (self.move_top, CAMERA_MOVE_TOP_FLAG),
+            (self.move_bottom, CAMERA_MOVE_BOTTOM_FLAG),
+            (self.move_right, CAMERA_MOVE_RIGHT_FLAG),
+            (self.move_left, CAMERA_MOVE_LEFT_FLAG),
+            (self.zoom_in, CAMERA_ZOOM_IN_FLAG),
+            (self.zoom_out, CAMERA_ZOOM_OUT_FLAG),
+        )
+        for handler, flag in assoc:
+            if len(handler.__code__.co_code) > 4: # handler has implemented ?
+                self.flags |= flag
+
+        print(self.flags)
+
     def get_snapshot(self):
         """Get an snapshot"""
         if self.snapshot_cache < time.time() - self.__snapshot_timestamp:
             self.make_snapshot()
 
         return self.__snapshot
+
+    def get_streaming(self):
+        """Use camera to make a snapshot"""
+        pass
+
+    def has_move_bottom(self):
+        """Camera can move to down?"""
+        return self.flags & CAMERA_MOVE_BOTTOM_FLAG
+
+    def has_move_left(self):
+        """Camera can move to left?"""
+        return self.flags & CAMERA_MOVE_LEFT_FLAG
+
+    def has_move_right(self):
+        """Camera can move to right?"""
+        return self.flags & CAMERA_MOVE_RIGHT_FLAG
+
+    def has_move_top(self):
+        """Camera can move to up?"""
+        return self.flags & CAMERA_MOVE_TOP_FLAG
+
+    def has_streaming(self):
+        """Camera can stream?"""
+        return self.flags & CAMERA_STREAMING_FLAG
+
+    def has_zoom_in(self):
+        """Camera can zoom in?"""
+        return self.flags & CAMERA_ZOOM_IN_FLAG
+
+    def has_zoom_out(self):
+        """Camera can zoom out?"""
+        return self.flags & CAMERA_ZOOM_OUT_FLAG
 
     def make_snapshot(self):
         """Use camera to make a snapshot"""
@@ -66,6 +124,15 @@ class CameraDevice(BaseDevice):
         """Decrease zoom"""
         pass
 
+    def get_motion_detection_score(self, image1, image2):
+        # get the difference between the two images
+        image_diff = ImageChops.difference(image1, image2)
+        # convert the resulting image into greyscale
+        image_diff = ImageOps.grayscale(image_diff)
+        # find the medium value of the grey pixels
+        image_stat = ImageStat.Stat(image_diff)
+        return image_stat.mean[0]
+
     def _make_snapshot_done(self, result):
         """Process after make_snapshot method"""
 
@@ -85,12 +152,3 @@ class CameraDevice(BaseDevice):
         self.__snapshot_timestamp = time.time()
         self.__snapshot = image
         return image
-
-    def get_motion_detection_score(self, image1, image2):
-        # get the difference between the two images
-        image_diff = ImageChops.difference(image1, image2)
-        # convert the resulting image into greyscale
-        image_diff = ImageOps.grayscale(image_diff)
-        # find the medium value of the grey pixels
-        image_stat = ImageStat.Stat(image_diff)
-        return image_stat.mean[0]
