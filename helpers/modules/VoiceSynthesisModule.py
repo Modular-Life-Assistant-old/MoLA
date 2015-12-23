6dba7d2934a8b90c85205f8d72639531ce28fbcf
Treemo <treemo@hotmail.fr>
2015-12-23 21:25:39 +0100

    VoiceSynthesisModule: fix crash

--- helpers/modules/VoiceSynthesisModule.py
+++ helpers/modules/VoiceSynthesisModule.py
@@ -4,7 +4,7 @@ from helpers.modules.NotificationModule import NotificationModule
 class VoiceSynthesisModule(NotificationModule):
     voice_quality = 0
 
-    def send(self, msg):
+    def send(self, msg, image=None, sound=None):
         """ Send message."""
         self.textToSpeak(msg)
 
